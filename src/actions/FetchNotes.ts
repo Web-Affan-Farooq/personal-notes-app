7954e51f6905d0e7df40407e128fbc5576a91878
Web-Affan-Farooq <affanamir903@gmail.com>
2025-10-03 06:30:19 -0700

    Added sorting

--- src/actions/FetchNotes.ts
+++ src/actions/FetchNotes.ts
@@ -1,27 +1,32 @@
-"use server"
+"use server";
 
 import db from "@/db";
 import { Note, note } from "@/db/schema";
 
-const FetchNotesAction = async () :Promise<{
-    message:string;
-    success:boolean;
-    notes?:Note[]
-}>=> {
-    try {
-        const notes = await db.select().from(note)
-        return {
-            success:true,
-            message:"",
-            notes:notes
-        }        
-    } catch (err) {
-        console.log(err);
-        return {
-            message:"An error occured",
-            success:false
-        }
-    }
-}
+const FetchNotesAction = async (): Promise<{
+  message: string;
+  success: boolean;
+  notes?: Note[];
+}> => {
+  try {
+    const notes = await db.select().from(note);
+    notes.sort((a, b) => {
+      const dateA = new Date(a.date as any).getTime();
+      const dateB = new Date(b.date as any).getTime();
+      return dateB - dateA;
+    });
+    return {
+      success: true,
+      message: "",
+      notes: notes,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      message: "An error occured",
+      success: false,
+    };
+  }
+};
 
-export default FetchNotesAction
+export default FetchNotesAction;
